946c93dbee761e608c15bb5092f05bcdfa2f7960
Len webel <leonard.webel@gmail.com>
2020-10-13 13:33:04 +0800

    add plugin source

---
+++ src/Plugins/Nop.Plugin.ExternalAuth.ExtendedAuth/ExternalAuthSettings.cs
@@ -0,0 +1,27 @@
+using Nop.Core.Configuration;
+
+namespace Nop.Plugin.ExternalAuth.ExtendedAuthentication
+{
+    public class ExternalAuthSettings : ISettings
+    {
+        public string FacebookClientId { get; set; }
+        public string FacebookClientSecret { get; set; }
+        public bool FacebookEnable { get; set; }
+
+        public string TwitterClientId { get; set; }
+        public string TwitterClientSecret { get; set; }
+        public bool TwitterEnable { get; set; }
+
+        public string GoogleClientId { get; set; }
+        public string GoogleClientSecret { get; set; }
+        public bool GoogleEnable { get; set; }
+
+        public string MicrosoftClientId { get; set; }
+        public string MicrosoftClientSecret { get; set; }
+        public bool MicrosoftEnable { get; set; }
+
+        public string LinkedInClientId { get; set; }
+        public string LinkedInClientSecret { get; set; }
+        public bool LinkedInEnable { get; set; }      
+    }
+}
